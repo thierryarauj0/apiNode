f0b33259d63036d6fe623af979621fdcac7d787f
Thierry Araujo de sousa <113550208+thierryarauj0@users.noreply.github.com>
2024-07-21 11:03:24 -0300

    api

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,15 @@ import express, {Request, Response} from 'express';
 
 const server = express();
 
+server.use(express.json())
+
 server.get( '/', (request: Request, response: Response) => {
     return response.status(200).json({message: 'Thi Bank Api is running!'});
 })
 
+server.post('/user' , (request: Request, response: Response) => {
+    const body = request.body;
+    console.log(body);
+     return response.status(201).json({message: 'User created successfully'});
+  })
 server.listen(5000, () => console.log('server on'))
