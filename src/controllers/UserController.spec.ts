7e6cffbbb0b7ae54ed3017d6c59c8061fffe883e
Thierry Araujo de sousa <113550208+thierryarauj0@users.noreply.github.com>
2024-07-21 15:33:17 -0300

    Incluir os testes unitários pendentes no controller
    Verificar a resposta de erro caso o usuário não informe o name
    Verificar se a função getAllusers está sendo chamada

--- src/controllers/UserController.spec.ts
+++ src/controllers/UserController.spec.ts
@@ -5,7 +5,8 @@ import { makeMockResponse } from "../__mocks__/mockResponse.mock";
 
 describe('UserController', () => {
     const mockUserService: Partial<UserService> = {
-        createUser: jest.fn()
+        createUser: jest.fn(),
+        getAllUsers: jest.fn().mockReturnValue([{ name: 'thi', email: 'thi@test.com' }])
     }
     
     const userController = new UserController(mockUserService as UserService);
@@ -13,13 +14,46 @@ describe('UserController', () => {
     it('Deve adicionar um novo usuário', () => {
         const mockRequest = {
             body: {
-                name: 'Nath',
-                email: 'nath@test.com'
+                name: 'thi',
+                email: 'thi@test.com'
             }
-        } as Request
-        const mockResponse = makeMockResponse()
-        userController.createUser(mockRequest, mockResponse)
-        expect(mockResponse.state.status).toBe(201)
-        expect(mockResponse.state.json).toMatchObject({ message: 'Usuário criado' })
-    })
-})
+        } as Request;
+        const mockResponse = makeMockResponse();
+        userController.createUser(mockRequest, mockResponse);
+        expect(mockResponse.state.status).toBe(201);
+        expect(mockResponse.state.json).toMatchObject({ message: 'Usuário criado' });
+    });
+
+    it('Deve retornar erro quando o nome não é fornecido', () => {
+        const mockRequest = {
+            body: {
+                email: 'thi@test.com'
+            }
+        } as Request;
+        const mockResponse = makeMockResponse();
+        userController.createUser(mockRequest, mockResponse);
+        expect(mockResponse.state.status).toBe(400);
+        expect(mockResponse.state.json).toMatchObject({ message: 'Bad request! Name obrigatório' });
+    });
+
+    it('Deve retornar erro quando o email não é fornecido', () => {
+        const mockRequest = {
+            body: {
+                name: 'thi'
+            }
+        } as Request;
+        const mockResponse = makeMockResponse();
+        userController.createUser(mockRequest, mockResponse);
+        expect(mockResponse.state.status).toBe(400);
+        expect(mockResponse.state.json).toMatchObject({ message: 'Bad request! Email obrigatório' });
+    });
+
+    it('Deve retornar todos os usuários', () => {
+        const mockRequest = {} as Request;
+        const mockResponse = makeMockResponse();
+        userController.getAllUsers(mockRequest, mockResponse);
+        expect(mockResponse.state.status).toBe(200);
+        expect(mockResponse.state.json).toEqual([{ name: 'thi', email: 'thi@test.com' }]);
+        expect(mockUserService.getAllUsers).toHaveBeenCalled();
+    });
+});
--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -13,9 +13,14 @@ export class UserController {
     createUser = (request: Request, response: Response): Response => {
         const user = request.body
 
-        if(!user.name){
+        if(!user.name ){
             return response.status(400).json({ message: 'Bad request! Name obrigatório'})
         }
+
+        if(!user.email ){
+            return response.status(400).json({ message: 'Bad request! Email obrigatório'})
+        }
+        
 
         this.userService.createUser(user.name, user.email)
         return response.status(201).json({ message: 'Usuário criado'})
