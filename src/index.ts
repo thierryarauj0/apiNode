0c2f30d7aa50366cbe93a39c6d7203b7f08a8697
Thierry Araujo de sousa <113550208+thierryarauj0@users.noreply.github.com>
2024-07-21 10:53:00 -0300

    server

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
-import express from 'express';
+import express, {Request, Response} from 'express';
 
 const server = express();
+
+server.get( '/', (request: Request, response: Response) => {
+    return response.status(200).json({message: 'Thi Bank Api is running!'});
+})
 
 server.listen(5000, () => console.log('server on'))
