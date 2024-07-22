1da111646b1515d3ce3faf43d725bc30ac4e4bea
Thierry Araujo de sousa <113550208+thierryarauj0@users.noreply.github.com>
2024-07-22 03:56:00 -0300

    adada

--- src/services/UserService.spec.ts
+++ src/services/UserService.spec.ts
@@ -9,21 +9,29 @@ const mockUserRepository = require('../repositories/UserRepository')
 
 describe('UserSerivce', () => {
     const userService = new UserService(mockUserRepository)
+    const mockUser={
+
+        id_user:'123456',
+        name:'renata',
+        email:'renata@dio.com',
+        password:'123'
+    }
 
     it('Deve adicionar um novo usuario' , async () => {
-        mockUserRepository.createUser = jest.fn().mockImplementation(() => Promise.resolve ({
-            id_user:'123456',
-            name:'renata',
-            email:'renata@dio.com',
-            password:'1234'
-        }))
+        mockUserRepository.createUser = jest.fn().mockImplementation(() => Promise.resolve  (mockUser))
         const response = await userService.createUser('renata', 'renata@dio.com' , '123');
         expect(mockUserRepository.createUser).toHaveBeenCalled()
         expect(response).toMatchObject({
             id_user:'123456',
             name:'renata',
             email:'renata@dio.com',
-            password:'1234'
+            password:'123'
         })
+    })
+
+    it('Devo retornar um usuario autenticado', async() => {
+        jest.spyOn(userService, 'getAuthenticatedUser').mockImplementation (() => Promise.resolve(mockUser))
+        const token = await userService.getToken('renata@dio.com' , '123')
+        expect(token).toBe('12345')
     })
 })
