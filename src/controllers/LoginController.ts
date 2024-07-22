3cc87fd2ebbfbd1ee2f4ef0047b70a0b6542ca27
Thierry Araujo de sousa <113550208+thierryarauj0@users.noreply.github.com>
2024-07-22 03:34:29 -0300

    loginn post com token

---
+++ src/controllers/LoginController.ts
@@ -0,0 +1,31 @@
+import {Request , Response} from 'express'
+import {sign} from 'jsonwebtoken'
+
+
+const user = {
+    id_user:'12345',
+    name:'Thierry Araujo',
+    email:'thi@dio.com',
+    password:'password'
+}
+
+export class LoginController{
+
+    login = async (request:Request , response:Response ) => {
+        const tokenData = {
+            name: user.name,
+            email: user.email
+        }
+
+        const tokenKey='12345'
+
+        const tokenOptions = {
+            subject: user.id_user,
+            expiresIn:'4h'
+        } 
+
+        const token = sign(tokenData, tokenKey, tokenOptions)
+        return response.status(200).json({token})
+    }
+
+}
--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,13 @@
 import express, { Router } from 'express';
 import { UserController } from './controllers/UserController';
+import { LoginController } from './controllers/LoginController';
 
 export const router = Router();
 const userController = new UserController();
+const loginController = new LoginController();
 
 router.post('/user', userController.createUser);
 router.get('/user/:id', userController.getUserById);
 router.delete('/user/:id', userController.deleteUser);
+
+router.post('/login', loginController.login)
